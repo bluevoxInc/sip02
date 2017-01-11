f0973c72bbfa5737577366fe672f21998e08a625
wnorman <bluevox.inc@gmail.com>
2017-01-11 14:33:12 -0500

    modify code to use Hibernate configuration. Recipe 2.3

--- sip02/src/main/java/org/bluevox/inc/ch02/model/Contact.java
+++ sip02/src/main/java/org/bluevox/inc/ch02/model/Contact.java
@@ -3,10 +3,30 @@
  */
 package org.bluevox.inc.ch02.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+
+import org.bluevox.inc.util.StringUtils;
+
 /**
  * @author wnorman
  *
  */
+@Entity
+@Table(name = "contact")
+@NamedQuery(
+		name = "findContactsByEmail",
+		query = "from Contact where email like :email")
 public class Contact {
 	private Long id;
 	private String lastName;
@@ -14,12 +34,18 @@ public class Contact {
 	private String middleInitial;
 	private String email;
 	
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	public Long getId() { return id; }
 	
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	@NotNull
+	@Length(min = 1, max = 40)
+	@Column(name = "last_name")
 	public String getLastName() {
 		return lastName;
 	}
@@ -28,6 +54,9 @@ public class Contact {
 		this.lastName = lastName;
 	}
 
+	@NotNull
+	@Length(min = 1, max = 40)
+	@Column(name = "first_name")
 	public String getFirstName() {
 		return firstName;
 	}
@@ -36,6 +65,8 @@ public class Contact {
 		this.firstName = firstName;
 	}
 
+	@Length(max = 1)
+	@Column(name = "mi")
 	public String getMiddleInitial() {
 		return middleInitial;
 	}
@@ -44,6 +75,8 @@ public class Contact {
 		this.middleInitial = middleInitial;
 	}
 
+	@Email
+	@Column
 	public String getEmail() {
 		return email;
 	}
@@ -52,6 +85,7 @@ public class Contact {
 		this.email = email;
 	}
 
+	@Transient
 	public String getFullName() {
 		String fullName = lastName + ", " + firstName;
 		if (! (middleInitial == null || "".equals(middleInitial.trim()))) {
--- sip02/src/main/java/org/bluevox/inc/ch02/service/impl/ContactServiceImpl.java
+++ sip02/src/main/java/org/bluevox/inc/ch02/service/impl/ContactServiceImpl.java
@@ -3,20 +3,12 @@
  */
 package org.bluevox.inc.ch02.service.impl;
 
-import static org.springframework.util.Assert.notNull;
-
 import java.util.List;
-import java.util.HashMap;
-
 import javax.inject.Inject;
-
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Service;
-import org.bluevox.inc.ch02.service.impl.ContactRowMapper;
+import org.springframework.transaction.annotation.Transactional;
 import org.bluevox.inc.ch02.model.Contact;
 import org.bluevox.inc.ch02.service.ContactService;
 
@@ -25,62 +17,38 @@ import org.bluevox.inc.ch02.service.ContactService;
  *
  */
 @Service
+@Transactional
 public class ContactServiceImpl implements ContactService {
 
-	private static final String CREATE_SQL =
-			"insert into contact (last_name, first_name, mi, email) " +
-			"values (:lastName, :firstName, :mi, :email)";
-	private static final String FIND_ALL_SQL =
-			"select id, last_name, first_name, mi, email from contact";
-	private static final String FIND_ALL_BY_EMAIL_LIKE =
-			"select id, last_name, first_name, mi, email from contact " +
-			"where email like :email";
-	private static final String FIND_ONE_SQL = 
-			"select id, last_name, first_name, mi, email from contact " +
-			"where id = :id";
-	private static final String UPDATE_SQL =
-			"update contact set last_name = :lastName, " +
-			"first_name = :firstName, mi = :mi, email = :email " +
-			"where id = :id";
-	private static final String DELETE_SQL =
-			"delete from contact where id = :id";
+	@Inject private SessionFactory sessionFactory;
 	
-	@Inject private NamedParameterJdbcOperations jdbcTemplate;
-	@Inject private ContactRowMapper contactRowMapper;
-	
-
 	/* (non-Javadoc)
 	 * @see org.bluevox.inc.ch02.service.ContactService#createContact(org.bluevox.inc.ch02.model.Contact)
 	 */
 	@Override
 	public void createContact(Contact contact) {
-		SqlParameterSource params = new MapSqlParameterSource()
-			.addValue("lastName", contact.getLastName())
-			.addValue("firstName", contact.getFirstName())
-			.addValue("mi", contact.getMiddleInitial())
-			.addValue("email", contact.getEmail());
-		KeyHolder keyHolder = new GeneratedKeyHolder();
-		jdbcTemplate.update(CREATE_SQL, params, keyHolder, new String[] { "id" });
-		contact.setId(keyHolder.getKey().longValue());
+		getSession().save(contact);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.bluevox.inc.ch02.service.ContactService#getContacts()
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Contact> getContacts() {
-		return jdbcTemplate.query(
-				FIND_ALL_SQL, new HashMap<String, Object>(), contactRowMapper);
+		return getSession().createQuery("from Contact").list();
 	}
 
 	/* (non-Javadoc)
 	 * @see org.bluevox.inc.ch02.service.ContactService#getContactsByEmail(java.lang.String)
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Contact> getContactsByEmail(String email) {
-		SqlParameterSource params =
-				new MapSqlParameterSource("email", "%" + email + "%");
-		return jdbcTemplate.query(FIND_ALL_BY_EMAIL_LIKE, params, contactRowMapper);
+		return getSession()
+				.getNamedQuery("findContactsByEmail")
+				.setString("email", "%" + email + "%")
+				.list();
 	}
 
 	/* (non-Javadoc)
@@ -88,10 +56,7 @@ public class ContactServiceImpl implements ContactService {
 	 */
 	@Override
 	public Contact getContact(Long id) {
-		SqlParameterSource params = 
-				new MapSqlParameterSource("id", id);
-		return jdbcTemplate.queryForObject(
-				FIND_ONE_SQL, params, contactRowMapper);
+		return (Contact) getSession().get(Contact.class, id);
 	}
 
 	/* (non-Javadoc)
@@ -99,13 +64,7 @@ public class ContactServiceImpl implements ContactService {
 	 */
 	@Override
 	public void updateContact(Contact contact) {
-		SqlParameterSource params = new MapSqlParameterSource()
-			.addValue("id", contact.getId())
-			.addValue("lastName", contact.getLastName())
-			.addValue("firstName", contact.getFirstName())
-			.addValue("mi", contact.getMiddleInitial())
-			.addValue("email", contact.getEmail());
-		jdbcTemplate.update(UPDATE_SQL, params);
+		getSession().update(contact);
 	}
 
 	/* (non-Javadoc)
@@ -113,7 +72,11 @@ public class ContactServiceImpl implements ContactService {
 	 */
 	@Override
 	public void deleteContact(Long id) {
-		jdbcTemplate.update(DELETE_SQL, new MapSqlParameterSource("id", id));
+		getSession().delete(getContact(id));;
+	}
+	
+	private Session getSession() {
+		return sessionFactory.getCurrentSession();
 	}
 
 }
